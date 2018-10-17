3dffb6b1c7a89ef9c6bbdf25db7303f41834cb46
Natalia <nataliamh.88@gmail.com>
2018-10-17 16:54:14 -0300

    nueva dependencia para cambiar efecto y nav para mobile

---
+++ assets/lib/Effects/navbarMobile/mobile.js
@@ -0,0 +1,10 @@
+/*apre l'overlay */
+function openNav(){
+    document.getElementById("myNav").style.width ="100%";
+    
+  }
+  
+  /*chiude l'overlay */
+  function closeNav(){
+    document.getElementById("myNav").style.width="0%";
+  }
--- assets/lib/Effects/text/text.js
+++ assets/lib/Effects/text/text.js
@@ -48,9 +48,5 @@ var TxtRotate = function(el, toRotate, period) {
         new TxtRotate(elements[i], JSON.parse(toRotate), period);
       }
     }
-    // INJECT CSS
-    var css = document.createElement("style");
-    css.type = "text/css";
-    css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
-    document.body.appendChild(css);
+    
   };
