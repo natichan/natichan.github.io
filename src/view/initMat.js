0a197489edeff6368f40894ffb9ca16e9c29f992
Natalia <nataliamh.88@gmail.com>
2018-10-17 22:59:40 -0300

    estilo y menú nuevo en mobile

--- src/view/initMat.js
+++ src/view/initMat.js
@@ -4,7 +4,7 @@
 
   $(document).ready(function() {
     $(window).scroll(function() { // check if scroll event happened
-        if ($(document).scrollTop() > 20) { // check if user scrolled more than 50 from top of the browser window
+        if ($(document).scrollTop() > 40) { // check if user scrolled more than 50 from top of the browser window
             $(".navWebSection").css("background-color", "rgba(255, 255, 255, 0.98)");
         } else {
             $(".navWebSection").css("background-color", "transparent"); // if not, change it back to transparent
