42f429f9f8c930e153638228aa6acf1054b5c6ba
Natalia <nataliamh.88@gmail.com>
2018-10-18 11:24:53 -0300

    rutas

--- src/view/initMat.js
+++ src/view/initMat.js
@@ -2,12 +2,3 @@
     $('.materialboxed').materialbox();
   });
 
-  $(document).ready(function() {
-    $(window).scroll(function() { // check if scroll event happened
-        if ($(document).scrollTop() > 50) { // check if user scrolled more than 50 from top of the browser window
-            $(".navWebSection").css("background-color", "rgba(255, 255, 255, 0.98)");
-        } else {
-            $(".navWebSection").css("background-color", "transparent"); // if not, change it back to transparent
-        }
-    });
-});
