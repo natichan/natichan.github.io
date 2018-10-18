6a3e1592d955ecd254436b6d3d70840068fc6467
Natalia <nataliamh.88@gmail.com>
2018-10-18 10:49:20 -0300

    mobile nav cierra al elegir seccion

--- assets/lib/Effects/navbarMobile/mobile.js
+++ assets/lib/Effects/navbarMobile/mobile.js
@@ -3,8 +3,8 @@ function openNav(){
     document.getElementById("myNav").style.width ="100%";
     
   }
-  
-  /*chiude l'overlay */
+    /*chiude l'overlay */
   function closeNav(){
     document.getElementById("myNav").style.width="0%";
+
   }
