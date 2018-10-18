d313939cf86dd6f4d346614062edcddc8d5a179e
Natalia <nataliamh.88@gmail.com>
2018-10-18 11:21:58 -0300

    rutas

--- assets/lib/Effects/navbarMobile/mobile.js
+++ assets/lib/Effects/navbarMobile/mobile.js
@@ -1,10 +1,12 @@
 /*apre l'overlay */
 function openNav(){
     document.getElementById("myNav").style.width ="100%";
+    document.getElementById("mobileNavBtn").style.display ="none";
     
   }
     /*chiude l'overlay */
   function closeNav(){
     document.getElementById("myNav").style.width="0%";
+    document.getElementById("mobileNavBtn").style.display ="block";
 
   }
